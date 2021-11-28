96eff0e2796fedec1cd0d35c6b3bc0470d2ca91c
PC <PC@DESKTOP-VH0UJ4V>
2021-11-28 14:43:18 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp;
+            int k = 0;
+            Random rnd = new Random();
+            double[] mas = new double[12];
+            for (int i = 0; i < 12; i++)
+                mas[i] = rnd.Next(1000);
+            for (int i = 0; i < 12 - 1; i++)
+            {
+                for (int j = i + 1; j < 12; j++)
+                {
+                    if (mas[i] < mas[j])
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+                        k++;
+                    }
+                }
+            }
+            foreach (double num in mas)
+                Console.Write(num + " ");
+            Console.WriteLine("\nЧисло перестановок при сортировки: " + k);
+        }
+    }
+}
